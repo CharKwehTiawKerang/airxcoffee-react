43b8a5ac9f6bdbc28d106a381114de8d0a0389d7
Hariz Asyraf <takamagahara16@gmail.com>
2024-06-14 16:44:53 +0800

    ✨ Updating home page

--- src/components/Client/Client.jsx
+++ src/components/Client/Client.jsx
@@ -1,10 +1,66 @@
 import React from 'react'
 import './Client.scss'
 
+import starbucks from '../../assets/starbucks.svg'
+import liverpool from '../../assets/liverpool.svg'
+import dekra from '../../assets/dekra.svg'
+import mercedes from '../../assets/mercedes.svg'
+import porsche from '../../assets/porsche.svg'
+import intercontinental from '../../assets/intercontinental.svg'
+
+import milenio from '../../assets/milenio.svg'
+import business from '../../assets/business-insider.svg'
+import vogue from '../../assets/vogue.svg'
+import straitstime from '../../assets/the-straits-times.svg'
+import bangkok from '../../assets/BangkokPost.svg'
+import sprudge from '../../assets/sprudge.svg'
+
 const Client = () => {
   return (
     <div className='client'>
-        Client
+        <h2>Clients & Partners</h2>
+        <div className="clients">
+          <div className="item">
+            <img src={starbucks} alt="" />
+          </div>
+          <div className="item">
+            <img src={liverpool} alt="" />
+          </div>
+          <div className="item">
+            <img src={dekra} alt="" />
+          </div>
+          <div className="item">
+            <img src={mercedes} alt="" />
+          </div>
+          <div className="item">
+            <img src={porsche} alt="" />
+          </div>
+          <div className="item">
+            <img src={intercontinental} alt="" />
+          </div>
+        </div>
+
+        <h2>AirX Coffee In News</h2>
+        <div className="news-client">
+          <div className="item">
+            <img src={milenio} alt="" />
+          </div>
+          <div className="item">
+            <img src={business} alt="" />
+          </div>
+          <div className="item">
+            <img src={vogue} alt="" />
+          </div>
+          <div className="item">
+            <img src={straitstime} alt="" />
+          </div>
+          <div className="item">
+            <img src={bangkok} alt="" />
+          </div>
+          <div className="item">
+            <img src={sprudge} alt="" />
+          </div>
+        </div>
     </div>
   )
 }
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './Map.scss'
 
+import location from '../../assets/viet-nam-map_22.jpg'
+
 const Map = () => {
   return (
     <div className='map'>
-        Map
+        <span>
+          私たちは「コーヒーかす」からさまざまなライフスタイル製品を作り、より高品質な製品を生産するだけでなく、
+          環境問題の解決に寄与し、その精神を世界に発信します。
+        </span>
+        <div className="location-container">
+        <div className="location-info">
+            hi
+          </div>
+          <img src={location} alt="" />
+          
+        </div>
     </div>
   )
 }
